0401a5e5dd48fe11ce1cd8d481412cb0b5bcae0a
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-05-28 10:55:27 -1000

    Route live test gates through Bumblebee config

--- test/live/model_forward_live_test.exs
+++ test/live/model_forward_live_test.exs
@@ -4,6 +4,6 @@ defmodule CrucibleBumblebee.Live.ModelForwardLiveTest do
   @moduletag :live_cpu_heavy
 
   test "live model forward is opt-in" do
-    assert System.get_env("CRUCIBLE_BUMBLEBEE_LIVE") in ["1", "true"]
+    assert CrucibleBumblebee.Config.live_enabled?()
   end
 end
--- test/live/model_generation_live_test.exs
+++ test/live/model_generation_live_test.exs
@@ -4,6 +4,6 @@ defmodule CrucibleBumblebee.Live.ModelGenerationLiveTest do
   @moduletag :live_cpu_heavy
 
   test "live model generation is opt-in" do
-    assert System.get_env("CRUCIBLE_BUMBLEBEE_LIVE") in ["1", "true"]
+    assert CrucibleBumblebee.Config.live_enabled?()
   end
 end
